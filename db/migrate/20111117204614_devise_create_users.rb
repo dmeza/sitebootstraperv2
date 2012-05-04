12e7fa339f8f586be039e0134844020a7b1eec65
Gustavo Gonzalez <gustavo@martintechlabs.com>
2012-05-04 14:36:30 -0500

    Changed migration for the new devise version

--- db/migrate/20111117204614_devise_create_users.rb
+++ db/migrate/20111117204614_devise_create_users.rb
@@ -10,12 +10,33 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :photo_content_type
       t.integer :photo_file_size
       t.boolean :is_admin, :default => false
-      t.database_authenticatable :null => false
-      t.encryptable
-      t.recoverable
-      t.confirmable
-      t.rememberable
-      t.trackable
+      ## authenticatable
+      t.string :email, :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
+      ## encryptable
+      t.string :password_salt
+
+      ## recoverable
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ##  confirmable
+      t.string :confirmation_token
+      t.string :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string :unconfirmed_email
+
+      ## rememberable
+      t.datetime :rememeber_created_at
+
+      ## trackable
+      t.integer :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
+
       t.timestamps
     end
 
